a6e7d70c8d895ef07e588c60814610f5cd81a537
RenataGusmao <renatagusmaoads@gmail.com>
2025-10-09 17:02:48 -0300

    Update sistemas_evento.py
    Atualizações de acordo com o sprint 2

--- sistemas_evento.py
+++ sistemas_evento.py
@@ -1,11 +1,13 @@
 from datetime import date
-from evento import Evento                 # antes: core.models.evento
-from participante import Participante     # antes: core.models.participante
+from evento import Evento
+from participante import Participante
 
 class SistemaEventos:
+    
     def __init__(self, repo):
         self._repo = repo
 
+    
     def criar_evento(self, nome, data_evento, local, capacidade_max, categoria, preco):
         if data_evento < date.today():
             return False, "A data do evento não pode ser anterior à data atual."
@@ -26,4 +28,44 @@ class SistemaEventos:
         evento = self.obter_evento(id_evento)
         if not evento:
             return False, "Evento não encontrado."
-        return evento.inscrever(participante)
+        ok, msg = evento.inscrever(participante)
+        if ok:
+            self._repo.salvar_evento(evento)
+        return ok, msg
+
+   
+    def cancelar_inscricao(self, id_evento: int, email: str):
+        evento = self.obter_evento(id_evento)
+        if not evento:
+            return False, "Evento não encontrado."
+        ok, msg = evento.cancelar_inscricao(email)
+        if ok:
+            self._repo.salvar_evento(evento)
+        return ok, msg
+
+    def checkin(self, id_evento: int, email: str):
+        evento = self.obter_evento(id_evento)
+        if not evento:
+            return False, "Evento não encontrado."
+        ok, msg = evento.checkin(email)
+        if ok:
+            self._repo.salvar_evento(evento)
+        return ok, msg
+
+    def relatorio_total_inscritos(self, id_evento: int) -> (bool, str | int):
+        evento = self.obter_evento(id_evento)
+        if not evento:
+            return False, "Evento não encontrado."
+        return True, evento.total_inscritos()
+
+    def relatorio_eventos_com_vagas(self):
+        # retorna lista de eventos com vagas > 0
+        return [e for e in self.listar_eventos() if e.vagas_disponiveis > 0]
+
+    def relatorio_receita_evento(self, id_evento: int) -> (bool, str | float):
+        evento = self.obter_evento(id_evento)
+        if not evento:
+            return False, "Evento não encontrado."
+        return True, evento.receita_total()
+
+
